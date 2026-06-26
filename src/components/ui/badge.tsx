ff4c0c6a5d9f119b75afb8348738f9c642833590
ccopelotti-dev <c.copelotti@gmail.com>
2026-06-26 08:30:45 -0300

    Update badge.tsx

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -29,4 +29,13 @@ const badgeVariants = cva(
 )
 
 export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivEle
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  )
+}
+
+export { Badge, badgeVariants }
